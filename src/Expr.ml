451591b349661bee1bcbd209e42a1cb2be632c22
taery <lavrenko.a@gmail.com>
2018-02-18 14:55:26 +0300

    support eval

--- src/Expr.ml
+++ src/Expr.ml
@@ -36,13 +36,13 @@ let update x v s = fun y -> if x = y then v else s y
 let s = update "x" 1 @@ update "y" 2 @@ update "z" 3 @@ update "t" 4 empty
 
 (* Some testing; comment this definition out when submitting the solution. *)
-let _ =
+(*let _ =
   List.iter
     (fun x ->
        try  Printf.printf "%s=%d\n" x @@ s x
        with Failure s -> Printf.printf "%s\n" s
     ) ["x"; "a"; "y"; "z"; "t"; "b"]
-
+*)
 (* Expression evaluator
 
      val eval : state -> expr -> int
@@ -50,5 +50,28 @@ let _ =
    Takes a state and an expression, and returns the value of the expression in 
    the given state.
 *)
-let eval = failwith "Not implemented yet"
+let rec eval st e : int = 
+  match e with 
+  | Const (c) -> c
+  | Var (v) -> st v 
+  | Binop (op, left, right) -> 
+  let e_l = eval st left in
+  let e_r = eval st right in 
+  let to_bool cond = if cond == 0 then false else true in
+  let to_int b = if b then 1 else 0 in
+  match op with
+    | "!!" -> to_int (to_bool e_l || to_bool e_r)
+    | "&&" -> to_int (to_bool e_l && to_bool e_r)
+    | "==" -> to_int (e_l == e_r)
+    | "!=" -> to_int (e_l != e_r)
+    | ">=" -> to_int (e_l >= e_r)
+    | "<=" -> to_int (e_l <= e_r)
+    | ">" -> to_int (e_l > e_r)
+    | "<" -> to_int (e_l < e_r)
+    | "+" -> e_l + e_r
+    | "-" -> e_l - e_r
+    | "*" -> e_l * e_r
+    | "/" -> e_l / e_r
+    | "%" -> e_l mod e_r
+    | _ -> failwith (Printf.sprintf "Unknown operation %s" op);;
                     
